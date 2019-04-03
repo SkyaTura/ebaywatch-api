b9720c79327eb4b2ce5c39e9ff29ff31d0d99ad3
Antonio Eduardo <antonio@skyatura.com.br>
2019-04-03 11:55:47 -0300

    ✏️ Remove defaults

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -4,9 +4,9 @@ import { mongo } from './vars'
 export default () =>
   mongoose
     .connect(mongo.uri || 'mongodb://localhost/admin', {
-      user: mongo.user || 'root',
-      pass: mongo.pass || 'example',
-      dbName: mongo.dbName || 'oruby',
+      user: mongo.user,
+      pass: mongo.pass,
+      dbName: mongo.dbName,
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
